956305ca86fb28cc2b69107f3b05aa19b5c790e7
yunhaveto <yunhaveto@gmail.com>
2019-12-16 18:02:52 +0200

    fixed font size and label

---
+++ app/src/main/java/com/android/odessatourguide/BuildingsInfoGrid/GridElement.java
@@ -0,0 +1,48 @@
+package com.android.odessatourguide.BuildingsInfoGrid;
+
+public class GridElement {
+
+    private Integer image;
+    private String name;
+    private String description;
+    private String link;
+
+    GridElement(Integer image, String name, String description, String link) {
+        this.image = image;
+        this.name = name;
+        this.description = description;
+        this.link = link;
+    }
+
+    public Integer getImage() {
+        return image;
+    }
+
+    public void setImage(Integer image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
---
+++ app/src/main/java/com/android/odessatourguide/BuildingsInfoGrid/ImageAdapter.java
@@ -0,0 +1,83 @@
+package com.android.odessatourguide.BuildingsInfoGrid;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.odessatourguide.R;
+
+import java.util.ArrayList;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private LayoutInflater lInflater;
+    private ArrayList<GridElement> gridElements;
+
+    ImageAdapter(Context context) {
+        mContext = context;
+        lInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        gridElements = fillArray(gridElements);
+    }
+
+    private ArrayList<GridElement> fillArray(ArrayList<GridElement> gridElements) {
+        gridElements = new ArrayList<>();
+        gridElements.add(new GridElement(R.drawable.grid_guk,
+                "Одесский национальный политехничный университет",
+                mContext.getString(R.string.grid_onpu),
+                mContext.getString(R.string.onpu_link)));
+        gridElements.add(new GridElement(R.drawable.grid_gov,
+                "Одесский городской совет",
+                mContext.getString(R.string.grid_town_hall),
+                mContext.getString(R.string.town_hall_link)));
+        gridElements.add(new GridElement(R.drawable.grid_kirha,
+                "Лютеранская церковь (Кирха)",
+                mContext.getString(R.string.grid_church),
+                mContext.getString(R.string.church_link)));
+        gridElements.add(new GridElement(R.drawable.grid_opera,
+                "Одесский государственный академический театр оперы и балета",
+                mContext.getString(R.string.grid_opera),
+                mContext.getString(R.string.opera_link)));
+        return gridElements;
+    }
+
+    @Override
+    public int getCount() {
+        return gridElements.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return gridElements.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private GridElement getGridElement(int position) {
+        return ((GridElement) getItem(position));
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = convertView;
+        if (view == null) {
+            view = lInflater.inflate(R.layout.grid_element, parent, false);
+        }
+
+        GridElement p = getGridElement(position);
+        ((TextView) view.findViewById(R.id.grid_item_text)).setText(p.getName());
+        ((ImageView) view.findViewById(R.id.grid_item_image)).setImageResource(p.getImage());
+
+        return view;
+    }
+
+}
